759b6a1a4d6e3c488b5fe85b5bb559cb4122d761
Caleb L. Power <cpower@acmcentral.org>
2019-06-15 16:54:05 -0500

    added first-timestamp and last-timestamp method declarations in GlobalPlayer interface

--- src/main/java/com/meepcraft/backbone/api/entity/GlobalPlayer.java
+++ src/main/java/com/meepcraft/backbone/api/entity/GlobalPlayer.java
@@ -1,5 +1,6 @@
 package com.meepcraft.backbone.api.entity;
 
+import java.util.Date;
 import java.util.UUID;
 
 import org.json.JSONObject;
@@ -13,14 +14,18 @@ public interface GlobalPlayer {
     BANNED
   }
   
-  public GlobalLocation getLastKnownLocation();
-  
   public UUID getUUID();
   
   public String getIGN();
   
   public Status getStatus();
   
+  public GlobalLocation getLastKnownLocation();
+  
+  public Date getFirstSeenTimestamp();
+  
+  public Date getLastSeenTimestamp();
+    
   public JSONObject serialize();
   
   public boolean equals(GlobalPlayer player);
